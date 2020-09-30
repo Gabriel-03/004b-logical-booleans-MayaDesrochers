bba9929fe44bc5d58df7405af09e5a414852251b
MayaDesrochers <71406875+MayaDesrochers@users.noreply.github.com>
2020-09-29 20:15:02 -0700

    Update and rename task.1py to task1.py

--- task1.py
+++ task1.py
@@ -15,3 +15,13 @@ Outputs:
 That is within normal Earth orbit.
 That is not within normal Earth orbit.
 """
+number=input("Enter number")
+number=float(number)
+
+a=0.9759
+b=1.016
+
+if number>=a and number<=b:
+    print("That is within normal Earth orbit.")
+else:
+    print("That is not within normal Earth orbit.")
