85a34f894cb7b8548262d9560fea49b70f4e6009
MayaDesrochers <71406875+MayaDesrochers@users.noreply.github.com>
2020-10-02 11:35:03 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -32,7 +32,7 @@ import math
 number=input("Enter number")
 number=float(number)
 a=str(number)
-if math.sqrt(number)%1==0 and (number**1.0/3)%1==0:
+if math.sqrt(number)%1==0 and number**(1.0/3)%1==0:
     print(""+a+" "+"is both a perfect square and a perfect cube.")
 elif math.sqrt(number)%1==0:
     print(""+a+" "+"is only a perfect sqaure.")
