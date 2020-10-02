5c8fe6362ba7515f349b647a9753aef0724130e3
MayaDesrochers <71406875+MayaDesrochers@users.noreply.github.com>
2020-10-02 12:15:22 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -32,9 +32,12 @@ import math
 number=input("Enter number")
 number=float(number)
 a=str(number)
-if math.sqrt(number)%1==0 and number**(1.0/3)%1==0:
+if math.sqrt(number)%1==0 and round(number**(1.0/3),8)%1==0:
+    
+    
     print(""+a+" "+"is both a perfect square and a perfect cube.")
 elif math.sqrt(number)%1==0:
     print(""+a+" "+"is only a perfect sqaure.")
-elif (number**1.0/3)%1==0:
+elif number**(1.0/3)%1==0:
+    number=round(number,8)
     print(""+a+" "+"is only a perfect cube.")
